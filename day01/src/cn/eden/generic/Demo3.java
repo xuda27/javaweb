e0a4ea2afbfb6193ee089acc8a88d75d8e2b033d
xuda <xuda27@163.com>
2016-10-31 10:18:48 +0800

    finish day01

--- day01/src/cn/eden/generic/Demo3.java
+++ day01/src/cn/eden/generic/Demo3.java
@@ -11,8 +11,7 @@ import org.junit.Test;
 public class Demo3 {
 
 	/**
-	 * 自定义泛型方法
-	 * <T> 声明类型
+	 * 自定义泛型方法 <T> 声明类型
 	 * 
 	 * @param t
 	 * @return
@@ -23,14 +22,15 @@ public class Demo3 {
 
 	/**
 	 * 声明三个泛型类型
+	 * 
 	 * @param t
 	 * @param e
 	 * @param k
 	 */
-	public <T, E, K> void b (T t, E e, K k) {
-		
+	public <T, E, K> void b(T t, E e, K k) {
+
 	}
-	
+
 	@Test
 	public void test() {
 		System.out.println(a("aa"));
